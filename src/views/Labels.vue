149f664d655102e1ab83432d425d2133ee5d4542
simpson <13951719485@163.com>
2021-09-14 23:10:42 +0800

    创建 <layout/>

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Labels.vue
-    </div>
-    <Nav/>
-  </div>
+  <layout>
+    Labels.vue
+  </layout>
 </template>
 
 <script lang="ts">
@@ -14,15 +11,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
-  border: 1px solid red;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content{
-  border: 2px solid green;
-  overflow: auto;
-  flex-grow: 1;
-}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Statistics.vue
-    </div>
-    <Nav/>
-  </div>
+  <layout>
+    Statistics.vue
+  </layout>
 </template>
 
 <script lang="ts">
@@ -14,15 +11,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
-  border: 1px solid red;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content{
-  border: 2px solid green;
-  overflow: auto;
-  flex-grow: 1;
-}
+
 </style>
