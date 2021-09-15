e3a5787140cd0abd122ea4595c6827955e580366
simpson <13951719485@163.com>
2021-09-15 23:42:32 +0800

    添加 nav 样式

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,10 @@
   padding: 0;
   box-sizing: border-box;
 }
-
+body{
+  line-height: 1.5;
+  text-decoration: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
