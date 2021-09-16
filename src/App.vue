90b5091fb90eb17de5e458208d3af7c108bb777c
simpson <13951719485@163.com>
2021-09-16 19:47:08 +0800

    解决了 SVG 自带 fill 属性的问题

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,10 @@
 }
 body{
   line-height: 1.5;
+}
+a{
   text-decoration: none;
+  color: inherit;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="tally"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistical"/>
       统计
     </router-link>
@@ -27,18 +27,20 @@ nav{
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   flex-direction: row;
   font-size: 12px;
-  >.item{
+  > .item{
     padding: 2px 0;
   width: 33.33333%;
   display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-  }
   .icon{
     width: 32px;
     height: 32px;
   }
+  }
+  > .item.selected{
+  color: red;
+  }
 }
-
 </style>
