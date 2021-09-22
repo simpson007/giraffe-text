83662d01037d9dbb6766d6c0e4b3236d15b67327
simpson <13951719485@163.com>
2021-09-22 21:09:52 +0800

    安装 vue-class-component 的文档，使用 props 失败

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -2,36 +2,58 @@
   <div>
     <ul class="types">
       <li :class="type === '-' && 'selected'"
-      @click="selectType('-')"
-      >支出</li>
+          @click="selectType('-')"
+      >支出
+      </li>
       <li :class="type === '+' && 'selected'"
-      @click="selectType('+')"
-      >收入</li>
+          @click="selectType('+')"
+      >收入
+      </li>
     </ul>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Types',
-  props:['xxx'],
-  data(){
-    return{
-      type:'-' // '-'表示支出，'+'表示收入
-    }
-  },
-  mounted(){
-    console.log(this.xxx)
-  },
-  methods:{
-    selectType(type){ //type 只能是'-'和'+'中的一个
-      if(type !== '-' && type !== '+'){
-        throw new Error ('type is unknown')
-      }
-    this.type = type
-    }
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component({
+  props: {
+    propMessage: String
   }
-};
+})
+export default class Types extends Vue {
+  type = '-'; // '-'表示支出，'+'表示收入
+  helloMsg = 'Hello,' + this.proMessage;
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  selectType(type: string) { //type 只能是'-'和'+'中的一个
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
+    }
+    this.type = type;
+  }
+}
+// export default {
+//   name: 'Types',
+//   props:['xxx'],
+//   data(){
+//     return{
+//       type:'-' // '-'表示支出，'+'表示收入
+//     }
+//   },
+//   mounted(){
+//     console.log(this.xxx)
+//   },
+//   methods:{
+//     selectType(type){ //type 只能是'-'和'+'中的一个
+//       if(type !== '-' && type !== '+'){
+//         throw new Error ('type is unknown')
+//       }
+//     this.type = type
+//     }
+//   }
+// };
 </script>
 
 <style lang="scss" scoped>
