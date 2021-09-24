34650d8c4f7da12f77ea3b5d0eadae8c2699d282
simpson <13951719485@163.com>
2021-09-24 19:07:11 +0800

    完成 types 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -15,45 +15,18 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
-@Component({
-  props: {
-    propMessage: String
-  }
-})
+@Component
 export default class Types extends Vue {
-  type = '-'; // '-'表示支出，'+'表示收入
-  helloMsg = 'Hello,' + this.proMessage;
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  selectType(type: string) { //type 只能是'-'和'+'中的一个
+  type = '-';
+  selectType(type: string) {
     if (type !== '-' && type !== '+') {
       throw new Error('type is unknown');
     }
     this.type = type;
   }
 }
-// export default {
-//   name: 'Types',
-//   props:['xxx'],
-//   data(){
-//     return{
-//       type:'-' // '-'表示支出，'+'表示收入
-//     }
-//   },
-//   mounted(){
-//     console.log(this.xxx)
-//   },
-//   methods:{
-//     selectType(type){ //type 只能是'-'和'+'中的一个
-//       if(type !== '-' && type !== '+'){
-//         throw new Error ('type is unknown')
-//       }
-//     this.type = type
-//     }
-//   }
-// };
 </script>
 
 <style lang="scss" scoped>
