77fe560c7518690991aed23941929c2a1941d064
simpson <13951719485@163.com>
2021-10-01 10:12:16 +0800

    添加标签可跳转至编辑页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,7 +26,7 @@ const routes: Array<RouteConfig> = [
         component: Statistics
     },
     {
-        path: '/labels/edit:id',
+        path: '/labels/edit/:id',
         component: EditLabel
     },
     {
