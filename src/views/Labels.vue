62fa7847e79ab293e3b33a228ccd288c67dc3fa3
simpson <13951719485@163.com>
2021-10-04 22:15:51 +0800

    添加 EditLabel.vue 的样式，抽离了两个组件 Button 和 FormItem

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -2,13 +2,16 @@
   <layout>
     <div class="tags">
       <router-link class="tag" v-for="tag in tags" :key="tag.id"
-                                       :to="`/labels/edit/${tag.id}`">
-        <span>{{tag.name}}</span>
+                   :to="`/labels/edit/${tag.id}`">
+        <span>{{ tag.name }}</span>
         <icon name="right"></icon>
       </router-link>
     </div>
     <div class="createTag-wrapper">
-      <button class="createTag" @click="createTag">新建标签</button>
+      <Button class="createTag"
+              @click="createTag">
+        新建标签
+      </Button>
     </div>
   </layout>
 </template>
