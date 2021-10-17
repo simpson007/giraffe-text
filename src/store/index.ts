b7ceea7abe1021c3fea21e789988ee03120e4c92
simpson <13951719485@163.com>
2021-10-17 11:53:27 +0800

    修复一进页面就弹窗的 bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,6 +10,7 @@ const store = new Vuex.Store({
     state: {
         recordList: [],
         createRecordError: null,
+        createTagError: null,
         tagList: [],
         currentTag: undefined
     } as RootState,
@@ -61,22 +62,23 @@ const store = new Vuex.Store({
         },
         fetchTags(state) {
             state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
-            if(!state.tagList || state.tagList.length === 0){
-                store.commit('createTag','衣');
-                store.commit('createTag','食');
-                store.commit('createTag','住');
-                store.commit('createTag','行');
+            if (!state.tagList || state.tagList.length === 0) {
+                store.commit('createTag', '衣');
+                store.commit('createTag', '食');
+                store.commit('createTag', '住');
+                store.commit('createTag', '行');
             }
         },
         createTag(state, name: string) {
+            state.createTagError = null;
             const names = state.tagList.map(item => item.name);
             if (names.indexOf(name) >= 0) {
-                window.alert('标签重复了');
+                state.createTagError = new Error('tag name duplicated');
+                return;
             }
             const id = createId().toString();
             state.tagList.push({id, name: name});
             store.commit('saveTags');
-            window.alert('添加成功');
         },
         saveTags(state) {
             window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
